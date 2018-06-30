ce4f7c0c861b30ff118bd48163fc805381dd56ca
albertodiaz <albertodiaz@192.168.1.11>
2018-06-30 13:58:07 +0200

    Change word alegria to alergia

---
+++ FunctionalInterfaces/src/predicate/PredicateFI.java
@@ -0,0 +1,25 @@
+package predicate;
+
+import java.util.function.Predicate;
+
+public class PredicateFI {
+
+	public static void main(String[] args) {
+		printMessage("Alergia");
+	}
+
+	private static void printMessage(String message){
+
+		assert message != null : "el mensaje no puede ser nulo";
+		
+		Predicate<String> pred = x -> x.startsWith("a");
+
+		if (pred.test(message)) {
+			System.out.println("the message starts with a");
+		} else {
+			System.out.println("the message starts with other character");
+		}
+
+	}
+
+}
