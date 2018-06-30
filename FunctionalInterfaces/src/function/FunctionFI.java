64610ffb48e3bb532f2e8e0c438fd3b1bd68292c
albertodiaz <albertodiaz@192.168.1.11>
2018-06-30 14:09:18 +0200

    Add Function as functionalInterface

---
+++ FunctionalInterfaces/src/function/FunctionFI.java
@@ -0,0 +1,24 @@
+package function;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class FunctionFI {
+
+	public static void main(String[] args) {
+		
+		Integer[] list = {1,2,3,4};
+
+		printResult(Arrays.asList(list));
+	}
+
+	private static void printResult(List<Integer> list) {
+
+		Function<Integer, Integer> fun = x -> x * x;
+
+		list.stream().forEach(y -> System.out.println(fun.apply(y)));
+
+	}
+
+}
